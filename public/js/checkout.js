8d7cb1a75182c1318d12b9bc6928cbba5e40b91d
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2018-08-21 23:20:57 +0530

    add checkout template and make charges via stripe
    add checkout template and make charges via stripe

---
+++ public/js/checkout.js
@@ -0,0 +1,31 @@
+var stripe = Stripe('pk_test_RDdKHCFpNdIPzgbAtIZeUbnO');
+
+var $form = $("#checkout-form");
+$form.submit((event) => {
+  $form.find('button').prop('disabled', true);
+  Stripe.card.createToken({
+    number:  $('#card-number').val(),
+    cvc:     $('#card-cvc').val(),
+    exp_month: $('#card-expiry-year').val(),
+    exp_year: $('#address_zip').val(),
+    name: $('#card-name ')
+  }, stripeTokenHandler);
+  return false; // validate data before form submission to the server
+});
+
+function stripeTokenHandler(status, response) {
+  console.log(`MY RESPONSE ${response}`);
+  if(response.error) {
+    $('#charge-errors').text(response.error.message);
+    $('#charge-errors').removeClass('hidden');
+    $form.find('button').prop('disabled', false); // re-enable submission
+  } else { // token was created
+    const token = response.id;
+    console.log(`mytoken : ${token}`);
+    // insert token into the form so it gets submitted to the server
+    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+
+    // submit error
+    $form.get(0).submit();
+  }
+}
