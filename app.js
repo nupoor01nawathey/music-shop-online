b52b406e6daf1e355c978eeb71d786f886cf0076
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2018-08-26 14:29:46 +0530

    refactor app.js - separate user related routes
    refactor app.js - separate user related routes

--- app.js
+++ app.js
@@ -11,20 +11,19 @@ const express               = require('express'),
       cookieParser          = require('cookie-parser'),
       passport              = require('passport'),
       LocalStrategy         = require('passport-local'),
-      PassportLocalMongoose = require('passport-local-mongoose'),
-      isomorphic            = require('isomorphic-fetch');
+      PassportLocalMongoose = require('passport-local-mongoose');
 
-// include custom lib
-const seeder                = require('./public/js/seeder'),
-      Artist                = require('./models/artist'),
-      Cart                  = require("./models/cart");
+// include custom lib for passport
+const User                  = require('./models/user');
+
 
 // include routes
 const indexRouter           = require('./routes/index')
       songsRouter           = require('./routes/songs'),
       addToCartRouter       = require('./routes/addToCart'),
       shoppingCartRouter    = require('./routes/shoppingCart'),
-      checkoutRouter        = require('./routes/checkout');
+      checkoutRouter        = require('./routes/checkout'),
+      userRouter            = require('./routes/user');
 
 // setup express app
 const app =  express();
@@ -72,51 +71,8 @@ app.use('/', songsRouter);
 app.use('/add-to-cart', addToCartRouter);
 app.use('/shopping-cart', shoppingCartRouter);
 app.use('/checkout', checkoutRouter);
+app.use('/user', userRouter);
 
-
-app.get('/user/signup', (req, res) => {
-    res.render('user/signup');
-});
-
-app.post('/user/signup', (req, res) => {
-    const username = req.body.username,
-          password = req.body.password;
-
-    User.register(new User({username: username}), password, (err, user) => {
-        if(err) {
-            return res.render('user/signup');
-        }
-        passport.authenticate("local")(req, res, () => {
-            res.redirect('/user/signin');
-        });
-    });
-});
-
-app.get('/user/signin', (req, res) => {
-      res.render('user/signin');
-});
-
-app.post('/user/signin', passport.authenticate("local", {
-    successRedirect: '/user/profile',
-    failureRedirect: '/user/signup'
-}));
-
-app.get('/user/signout', isLoggedIn, (req, res) => { // force user login before hitting signout page
-    req.session.cart = null; // empty cart once logged out
-    req.logout();
-    res.redirect('/user/signin');
-});
-
-app.get('/user/profile', isLoggedIn, (req, res) => {
-    res.render('user/profile'); // force user login before hitting profile page
- });
-
-function isLoggedIn(req, res, next) {
-    if(req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("/user/signin");
-}
 
 const PORT = process.env.PORT || 3000 ; // TODO get it from config depending on dev/prod env
 app.listen(PORT, () => {
